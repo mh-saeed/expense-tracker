51e8b5dfcbe88ce37a387f3ef4169acd20197a71
mh-saeed <saeedkhan111244@gmail.com>
2021-08-25 23:35:39 +0500

    form component Completed

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { View } from "react-native";
-import { Input, Button , Icon} from "react-native-elements";
-
-// import Icon from "react-native-vector-icons/Ionicons";
+import { Input, Button, Icon } from "react-native-elements";
+import { GlobalContext } from "../../context/GlobalState";
 
 const FormInput = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const { addTransactions } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    if (text.length === 0 || amount === 0) {
+      // e.preventDefault();
+      alert(`please fill the fields!`);
+    } else {
+      // e.preventDefault();
+      const newTransaction = {
+        id: Math.floor(Math.random() * 100000000),
+        text,
+        amount: +amount,
+      };
+
+      addTransactions(newTransaction);
+
+      // updating field / clearing after form submission
+      setText(" ");
+      setAmount(0);
+    }
+  };
+
   return (
     <View>
       <Input
         placeholder="TRANSACTION NAME"
+        value={text}
+        onChangeText={setText}
         leftIcon={
           <Icon
             name="document-text-outline"
@@ -19,7 +45,10 @@ const FormInput = () => {
         }
       />
       <Input
+        keyboardType="numeric"
         placeholder="TRANSACTION AMOUNT"
+        value={amount}
+        onChangeText={setAmount}
         leftIcon={
           <Icon
             name="attach-money"
@@ -29,7 +58,7 @@ const FormInput = () => {
           />
         }
       />
-      <Button title="Outline button" type="outline" />
+      <Button title="ADD TRANSACTION" onPress={(e) => onSubmit(encodeURI)} />
     </View>
   );
 };
