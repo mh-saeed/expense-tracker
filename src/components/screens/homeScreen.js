2a2cb11eb3242d4435179e57488e21a2db1f4893
mh-saeed <saeedkhan111244@gmail.com>
2021-08-25 13:57:13 +0500

    tabs navigation added

--- src/components/screens/homeScreen.js
+++ src/components/screens/homeScreen.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-import StatusCard from "../statusCard/statusCard";
+import TransactionScreen from "./transactionScreen"; 
+import TransHistoryScreen from "./transHistoryScreen";
+
+
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+const Tab = createMaterialTopTabNavigator();
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-     <StatusCard/>
-      <Button onPress={() => navigation.navigate("About")} title="About" />
+      <Tab.Navigator>
+        <Tab.Screen name="Transaction" component={TransactionScreen} />
+        <Tab.Screen name="History" component={TransHistoryScreen} />
+      </Tab.Navigator>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 0.5,
-    alignItems: "center", 
-    justifyContent: "center"
-   },
+  container: {
+    flex: 1,
+  },
 });
 
 export default HomeScreen;
