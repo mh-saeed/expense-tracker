24ee12552e0a441d30ed57336ae5f873f6a9c832
mh-saeed <saeedkhan111244@gmail.com>
2021-08-25 13:59:00 +0500

    form component created

---
+++ src/components/form/form.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View } from "react-native";
+import { Input } from "react-native-elements";
+
+const FormInput = () => {
+  return (
+    <View>
+      <Input placeholder="BASIC INPUT" />
+      <Input placeholder="BASIC INPUT" />
+    </View>
+  );
+};
+
+
+export default FormInput;
