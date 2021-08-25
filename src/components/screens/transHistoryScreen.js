68eceb9ab692d44752fe9837acefb3bcaf5e867e
mh-saeed <saeedkhan111244@gmail.com>
2021-08-25 23:37:09 +0500

    Transaction history screen completed

--- src/components/screens/transHistoryScreen.js
+++ src/components/screens/transHistoryScreen.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext } from "react";
+import { GlobalContext, GlobalProvider } from "../../context/GlobalState";
+import { ScrollView } from "react-native";
+import { ListItem, Icon } from "react-native-elements";
 
 const TransHistoryScreen = () => {
-    return (
-       <View>
-           <Text>Transaction History</Text>
-       </View>
-    )
-}
+  const { transactions, deleteTransactions } = useContext(GlobalContext);
+  return (
+    <ScrollView>
+     
+        {transactions.map((transaction) => (
+          <ListItem
+            key={transaction.id}
+            style={{
+              borderLeftColor: `${
+                transaction.amount < 0 ? "#e84118" : "#4cd137"
+              }`,
+              borderLeftWidth: 5,
+              marginTop: 4,
+            }}
+            bottomDivider
+          >
+            <ListItem.Content>
+              <ListItem.Title>
+                {transaction.amount < 0 ? `-` : `+`}$
+                {Math.abs(transaction.amount)}
+              </ListItem.Title>
+              <ListItem.Subtitle>{transaction.text}</ListItem.Subtitle>
+            </ListItem.Content>
+            <ListItem color="black">
+              <Icon
+                name="delete-forever"
+                type="material-icons"
+                size={28}
+                color="#EA2027"
+                onPress={() => deleteTransactions(transaction.id)}
+              />
+            </ListItem>
+          </ListItem>
+        ))}
+    </ScrollView>
+  );
+};
 
-export default TransHistoryScreen
+export default TransHistoryScreen;
